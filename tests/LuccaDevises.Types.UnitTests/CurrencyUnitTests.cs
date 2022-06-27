fb2f21ce6e774c35088046d548c226a04ea9cd19
ML <lepecq@hotmail.com>
2022-06-27 17:59:00 +0200

    Ajout des tests pour Equals

--- tests/LuccaDevises.Types.UnitTests/CurrencyUnitTests.cs
+++ tests/LuccaDevises.Types.UnitTests/CurrencyUnitTests.cs
@@ -31,5 +31,21 @@
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => new Currency("AB"));
         }
+
+        [Fact]
+        public void Equals_ShouldBeFalse_WithNullValue()
+        {
+            var instance = new Currency("EUR");
+
+            Assert.False(instance.Equals(null));
+        }
+
+        [Fact]
+        public void Equals_ShouldBeTrue_WithSameValues()
+        {
+            var instance = new Currency("EUR");
+
+            Assert.True(instance.Equals(new Currency("EUR")));
+        }
     }
 }
